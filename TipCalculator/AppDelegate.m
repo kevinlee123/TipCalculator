f501e2d5eace8d7e2b2437898f879979f82a082d
Kevin Lee <kevlee@zynga.com>
2013-10-01 22:32:31 -0700

    Initial commit for TipCalculator.

--- TipCalculator/AppDelegate.m
+++ TipCalculator/AppDelegate.m
@@ -7,13 +7,19 @@
 //
 
 #import "AppDelegate.h"
+#import "TipViewController.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
     // Override point for customization after application launch.
+    TipViewController *tipViewController = [[TipViewController alloc] init];
+    UINavigationController *navController =[[UINavigationController alloc] initWithRootViewController:tipViewController];
+    self.window.rootViewController = navController;
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ TipCalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  TipCalculator
+//
+//  Created by Kevin Lee on 10/1/13.
+//  Copyright (c) 2013 Kevin Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
